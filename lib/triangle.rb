629eb1792b4f5673c0c8a52478d93b1a4bdc6af4
Chrissy Hunt <chrissy.hunt@gmail.com>
2017-10-28 16:42:43 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -16,7 +16,7 @@ class Triangle
       :equilateral
     elsif ((length_1 == length_2 ) && (length_2 != length_3)) || ((length_2 == length_3) && (length_3 != length_1)) || ((length_1 == length_3) && (length_3 != length_2))
       :isosceles
-    elsif (length_1 + length_2 <= length_3 || length_2 + length_3 <= length_1 || length_1 + length_3 <= length_2) || length_1 == 0 || length_2 == 0 || length_3 == 0
+    elsif (length_1 + length_2 <= length_3 || length_2 + length_3 <= length_1 || length_1 + length_3 <= length_2)
       raise TriangleError
     else
       :scalene
